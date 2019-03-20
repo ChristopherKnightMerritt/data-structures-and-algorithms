f03e6e46bde8fda7747d2df065a3732107356064
christopherknightmerritt <chris.merritt95@gmail.com>
2019-03-19 17:52:25 -0700

    fixed linter errors

--- code-challenges/401/challenges/array-shift/__test__/array-shift.test.js
+++ code-challenges/401/challenges/array-shift/__test__/array-shift.test.js
@@ -3,25 +3,25 @@
 const arrayShift = require('../lib/array-shift.js');
 
 describe('array-shift.js', () => {
-    it('should insert a value in the middle if the first value is an array.', () => {
-        expect(arrayShift.shift([1,2,3,4], 1)).toEqual([1,2,1,3,4]);
-    }),
-    it('should fail with only one input', ()=> {
-        expect(arrayShift.shift([1,2,3,4])).toBeNull();
-    }),
-    it('should fail with too many inputs', ()=> {
-        expect(arrayShift.shift([1,2,3,4], 3 ,6)).toBeNull();
-    })
-    it('should return null if the first value is a string', () => {
-        expect(arrayShift.shift("Hi",2)).toBeNull();
-    }),
-    it('should return null if the first value is an object', () => {
-        expect(arrayShift.shift({},2)).toBeNull();
-    }),
-    it('should return null if the first value is a boolean', () => {
-        expect(arrayShift.shift(true,2)).toBeNull();
-    }),
-    it('should return null if the first value is a number', () => {
-        expect(arrayShift.shift(1,2)).toBeNull();
-    })
+  it('should insert a value in the middle if the first value is an array.', () => {
+    expect(arrayShift.shift([1,2,3,4], 1)).toEqual([1,2,1,3,4]);
+  }),
+  it('should fail with only one input', ()=> {
+    expect(arrayShift.shift([1,2,3,4])).toBeNull();
+  }),
+  it('should fail with too many inputs', ()=> {
+    expect(arrayShift.shift([1,2,3,4], 3 ,6)).toBeNull();
+  }),
+  it('should return null if the first value is a string', () => {
+    expect(arrayShift.shift('Hi',2)).toBeNull();
+  }),
+  it('should return null if the first value is an object', () => {
+    expect(arrayShift.shift({},2)).toBeNull();
+  }),
+  it('should return null if the first value is a boolean', () => {
+    expect(arrayShift.shift(true,2)).toBeNull();
+  }),
+  it('should return null if the first value is a number', () => {
+    expect(arrayShift.shift(1,2)).toBeNull();
+  });
 });
--- code-challenges/401/challenges/array-shift/lib/array-shift.js
+++ code-challenges/401/challenges/array-shift/lib/array-shift.js
@@ -9,25 +9,25 @@ let arrayShift = module.exports = {};
  * @returns {array}
  */
 arrayShift.shift = function(arr, x, c){
-    if(checkInput(arr, x, c) === null){ return null };
+  if(checkInput(arr, x, c) === null){ return null; }
 
-    let res = [];
-    let mid = Math.ceil(arr.length/2)-1;
+  let res = [];
+  let mid = Math.ceil(arr.length/2)-1;
 
-    for(let i = 0; i < arr.length; i++){
-        if(i < mid){
-            res[i] = arr[i];
-        }
-        if(i === mid){
-            res[i] = arr[i];
-            res[i + 1] = x;
-        }
-        else{
-            res[i + 1] = arr[i];
-        }
+  for(let i = 0; i < arr.length; i++){
+    if(i < mid){
+      res[i] = arr[i];
     }
-    return res;
-}
+    if(i === mid){
+      res[i] = arr[i];
+      res[i + 1] = x;
+    }
+    else{
+      res[i + 1] = arr[i];
+    }
+  }
+  return res;
+};
 /**
  * @param  {array} a
  * @param  {} b
@@ -35,7 +35,7 @@ arrayShift.shift = function(arr, x, c){
  * @returns {}
  */
 function checkInput(a, b, c){
-    if(!Array.isArray(a)){return null;}
-    if(!b){return null;}
-    if(c){return null;}
+  if(!Array.isArray(a)){return null;}
+  if(!b){return null;}
+  if(c){return null;}
 }
