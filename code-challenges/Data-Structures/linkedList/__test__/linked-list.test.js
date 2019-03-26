5243153ef498674408c6b2ee717e8d393a396db9
christopherknightmerritt <chris.merritt95@gmail.com>
2019-03-25 21:32:29 -0700

    completed code challenge

--- code-challenges/Data-Structures/linkedList/__test__/linked-list.test.js
+++ code-challenges/Data-Structures/linkedList/__test__/linked-list.test.js
@@ -31,3 +31,35 @@ describe('linked-list.js', ()=>{
     expect(newLL.print()).toEqual([200, 2, 1, 'test']);
   });
 });
+
+describe('linked list methods pt. 2', ()=>{
+  let newLL2 = new linkedList.LinkedList();
+  it('adds a node to the end of the list', ()=>{
+    newLL2.insert(2);
+    newLL2.insert(4);
+    newLL2.append('three');
+    expect(newLL2.print()).toEqual([4,2, 'three']);
+  }),
+  it('Can successfully add multiple nodes to the end of a linked list', () =>{
+    newLL2.append('end1');
+    newLL2.append('end2');
+    expect(newLL2.print()).toEqual([4,2,'three', 'end1', 'end2']);
+  }),
+  it('Can successfully insert a node before a node located in the middle of a linked list', () =>{
+    newLL2.insertBefore('three', 3);
+    expect(newLL2.print()).toEqual([4,2,3,'three', 'end1', 'end2']);
+  }),
+  it('Can successfully insert a node before the first node of a linked list', () =>{
+    newLL2.insertBefore(4, 'first');
+    expect(newLL2.print()).toEqual(['first', 4, 2, 3, 'three', 'end1', 'end2']);
+  }),
+  it('Can successfully insert after a node in the middle of the linked list', () =>{
+    newLL2.insertAfter('three', 9);
+    expect(newLL2.print()).toEqual(['first', 4, 2, 3, 'three', 9, 'end1', 'end2']);
+  }),
+  it('Can successfully insert a node after the last node of the linked list', () =>{
+    newLL2.insertAfter('end2', 0);
+    expect(newLL2.print()).toEqual(['first', 4, 2, 3, 'three', 9, 'end1', 'end2', 0]);
+    expect().toEqual();
+  });
+});
